9d802cef316f26ac302d6a3c5b9f46e86520cfaf
Rakibul Yeasin <ryeasin03@gmail.com>
2025-03-03 22:39:08 +0600

    CDK

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { DiscordBotStack } from '../lib/discord-bot-stack';
-import { DashboardStack } from '../lib/dashboard-stack';
-import { DynamoDBStack } from '../lib/dynamodb-stack';
+// import { DashboardStack } from '../lib/dashboard-stack';
+// import { DynamoDBStack } from '../lib/dynamodb-stack';
 
 const app = new cdk.App();
-new DynamoDBStack(app, 'DynamoDBStack');
+// new DynamoDBStack(app, 'DynamoDBStack');
 new DiscordBotStack(app, 'DiscordBotStack');
-new DashboardStack(app, 'DashboardStack');
+// new DashboardStack(app, 'DashboardStack');
 
 // app.synth();
