ee7964749897771ef0f075dbe669875c7850fe1d
Rakibul Yeasin <ryeasin03@gmail.com>
2025-03-05 02:32:34 +0600

    Fixed environment variable missing issue

--- apps/discord/src/config.ts
+++ apps/discord/src/config.ts
@@ -6,10 +6,7 @@
 // }
 
 export const config: Record<string, string | number> = {
-  token: "OTU0NTEzOTg2NjI2Mzg3OTY5.G8Jg0d.IXIaAYKiYHNWXazkicS4d6XdpHpjj9zRJaom5E",
+  token: process.env.TOKEN as string,
   color: 0x0099FF,
   thumbnail: process.env.THUMBNAIL as string,
 }
-
-console.log(config);
-console.log(process.env.TOKEN);
