2f9ac3a764c6ab98e61d670a6001598e1f0c20a4
Rakibul Yeasin <ryeasin03@gmail.com>
2025-03-05 02:15:18 +0600

    Fixed Dynamodb issue on bot

--- apps/discord/src/config.ts
+++ apps/discord/src/config.ts
@@ -6,11 +6,10 @@
 // }
 
 export const config: Record<string, string | number> = {
-  token: process.env.TOKEN as string,
+  token: "OTU0NTEzOTg2NjI2Mzg3OTY5.G8Jg0d.IXIaAYKiYHNWXazkicS4d6XdpHpjj9zRJaom5E",
   color: 0x0099FF,
   thumbnail: process.env.THUMBNAIL as string,
-  region: process.env.REGION as string,
-  endpoint: process.env.ENDPOINT as string,
-  accessKeyId: process.env.ACCESS_KEY_ID as string,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY as string
 }
+
+console.log(config);
+console.log(process.env.TOKEN);
--- apps/discord/src/database.ts
+++ apps/discord/src/database.ts
@@ -1,21 +1,29 @@
 import { DynamoDatabase } from '@discord/database';
+import type { IDBClientOptions } from '@discord/database/dist/types';
 
-// const database = new DynamoDatabase({
-//   region: process.env.REGION as string,
-//   endpoint: process.env.ENDPOINT as string,
-//   credentials: {
-//     accessKeyId: process.env.ACCESS_KEY_ID as string,
-//     secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
-//   },
-//   webhooksTableName: "Webhooks",
-//   regexTableName: "RegexPatterns",
-//   serversTableName: "DiscordServers",
-// });
+const region = process.env.REGION as string;
+const endpoint = process.env.ENDPOINT as string;
+const accessKeyId = process.env.ACCESS_KEY_ID as string;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY as string;
 
-const database = new DynamoDatabase({
+let config: IDBClientOptions = {
   webhooksTableName: "Webhooks",
   regexTableName: "RegexPatterns",
   serversTableName: "DiscordServers",
-});
+}
+
+if (region && endpoint && accessKeyId && secretAccessKey) {
+  config = {
+    ...config,
+    region: process.env.REGION as string,
+    endpoint: process.env.ENDPOINT as string,
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+    },
+  };
+}
+
+const database = new DynamoDatabase(config);
 
 export { database };
